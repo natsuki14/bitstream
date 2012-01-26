9e23d773143b176587f383fff8b9829c4cd5c9f6
Natsuki Kawai <natsuki.kawai@gmail.com>
2012-01-26 16:02:33 +0900

    Added a overload test case.

--- test/test-suite.rb
+++ test/test-suite.rb
@@ -1,5 +1,5 @@
 # Author:: Natsuki Kawai (natsuki.kawai@gmail.com)
-# Copyright:: Copyright 2011 Natsuki Kawai
+# Copyright:: Copyright (c) 2011, 2012 Natsuki Kawai
 # License:: 2-clause BSDL or Ruby's
 
 
@@ -7,6 +7,7 @@ require 'test/test-primitives'
 require 'test/test-array'
 require 'test/test-dynarray'
 require 'test/test-nesting'
+require 'test/test-overload'
 require 'test/test-simple-properties'
 require 'test/types/test-character'
 require 'test/types/test-integer'
